7c1c773e44ad039c0b433a6dd794361b51b533a8
jerry <jerrynwosu007@gmail.com>
2022-07-02 16:17:49 +0100

    label alias for url

--- models/labelModel.js
+++ models/labelModel.js
@@ -7,10 +7,15 @@ const labelSchema = mongoose.Schema(
       required: true,
       unique: true,
     },
+    alias: String,
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   },
   { timestamps: true }
 );
+
+labelSchema.pre('save', async function () {
+  this.alias = this.name.split(' ').join('-');
+});
 
 const Label = mongoose.model('Label', labelSchema);
 module.exports = Label;
